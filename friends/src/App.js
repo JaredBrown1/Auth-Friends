96d9c5d16ead0ba90fd4441f7efc0a0489d53064
Jared Brown <jaredbrown1@hotmail.com>
2020-02-13 21:47:29 -0800

    basic app setup / component files created

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import "./App.css";
+
+//components
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Friends</h1>
+        <nav>
+          <Link to="/login">Login</Link>
+          <br />
+          <Link to="/protected">Friends</Link>
+        </nav>
+        <Switch>
+          <Route exact path="/protected" />
+          <Route path="/login" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
