9575fdf93f79eebe7e1841fb5948a1560e5549e4
Jared Brown <jaredbrown1@hotmail.com>
2020-04-25 21:56:50 -0700

    app working

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import "./App.css";
+import styled from "styled-components";
+
+//components
+import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import FriendsList from "./components/Friends";
+
+const AppContainer = styled.div`
+  text-align: center;
+  max-height: 1000vh;
+`;
+
+const NavBar = styled.nav`
+  margin: 0 auto;
+  max-width: 300px;
+  display: flex;
+  justify-content: space-around;
+  background-color: #b4d3db;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppContainer className="App">
+        <h1>Friends</h1>
+        <NavBar>
+          <Link to="/login">Login</Link>
+          <br />
+          <Link to="/protected">Friends</Link>
+        </NavBar>
+        <Switch>
+          <ProtectedRoute exact path="/protected" component={FriendsList} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </AppContainer>
+    </Router>
   );
 }
 
